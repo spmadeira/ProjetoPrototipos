13c0d2545544c72878eea35e2c298ffb51d64072
Samuel Madeira <spmadeira@edu.unifor.br>
2019-11-22 14:03:15 -0300

    Começar a fazer Rule Tile

---
+++ Assets/test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class test : MonoBehaviour
+{
+    public Vector3Int position;
+    public Tilemap Tilemap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Tilemap.SetTile(position, null);
+        }
+    }
+}
