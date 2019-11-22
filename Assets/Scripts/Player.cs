37ea6a2052c68e8eaeb804b6007f80159ce34421
Samuel Madeira <spmadeira@edu.unifor.br>
2019-11-22 15:56:50 -0300

    Começar player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int MaxEnergy;
+    public int CurrentEnergy;
+    public int JumpEnergyCost;
+    public float MovementSpeed;
+    public float JumpForce;
+}
